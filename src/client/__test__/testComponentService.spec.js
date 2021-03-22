51958046bc97fee4032e272c0ef7fdf35f3d518d
sarahelhawary <sarah.elhwarii92@gmail.com>
2021-03-22 23:00:33 +0200

    write test for get result function

--- src/client/__test__/testComponentService.spec.js
+++ src/client/__test__/testComponentService.spec.js
@@ -1,1 +1,9 @@
-import { getResult } from '../js/services/component.service'
+import { ComponentService } from '../js/services/component.service'
+
+const componentService = new ComponentService()
+
+describe('Test get result functionality', () => {
+  test('test if get result defined or not', () => {
+    expect(componentService.getResult).toBeDefined()
+  })
+})
